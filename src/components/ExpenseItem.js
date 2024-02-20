5e3643c245cf75ab4ce804f475f6c67d235a0cf0
dmcdante007 <meshramswaraj98>
2024-02-20 19:11:27 +0530

    "Created-one-more-Custom-component"

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
         <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
     </div>
   );
 }
