135cfd67c7f277c7d02bffaf45d16e8e5aa252b8
dmcdante007 <meshramswaraj98>
2024-02-21 20:20:31 +0530

    "created-card-comp-to-wrap-the-items"

--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -1,16 +1,16 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExportDetails(props){
-    const title = props.title;
-    const location = props.location;
-    const amount = props.amount;
-    return(
-        <div className="expense-item__description">
-        <h2>{title}</h2>
-        <h2>{location}</h2>
-        <div className="expense-item__price">${amount}</div>
-      </div>
-    );
+function ExportDetails(props) {
+  const title = props.title;
+  const location = props.location;
+  const amount = props.amount;
+  return (
+    <div className="expense-item__description">
+      <h2>{title}</h2>
+      <h2>{location}</h2>
+      <div className="expense-item__price">${amount}</div>
+    </div>
+  );
 }
 
-export default ExportDetails
+export default ExportDetails;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-    </div>
+    </Card>
   );
 }
 
