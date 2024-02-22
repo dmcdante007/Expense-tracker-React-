f0c26bfcfe32024a0c7a85d71d60d64fd6f86478
dmcdante007 <meshramswaraj98>
2024-02-22 14:23:36 +0530

    "used-Arrow-funciton-inApp.js"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 
-
-function App() {
+//Used Arrow function to return
+const App = () => {
   const expenses = [
     {
       id: "e1",
