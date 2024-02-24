a50b6f8bcc454da42d29e599a39d52de3db1a1d3
dmcdante007 <meshramswaraj98>
2024-02-25 00:33:30 +0530

    "added-event-handler-on-changing-anthing"

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,19 +2,30 @@ import React from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = ()=> {
+    const titleChange = ()=>{
+        console.log('Title Changed!')
+    };
+    const amountChanged =()=>{
+        console.log('Amount channged!')
+    }
+
+    const dateChanged= ()=>{
+        console.log('Date changed!')
+    }
+
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Expense Title</label>
-                <input type="text"/>
+                <input type="text" onChange={titleChange}/>
             </div>
             <div className="new-expense__control">
                 <label>Expense Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input type="number" min="0.01" step="0.01" onChange={amountChanged}/>
             </div>
             <div className="new-expense__control">
                 <label>Expense Date</label>
-                <input type="date" min="2019-01-01" max="2025-01-01"/>
+                <input type="date" min="2019-01-01" max="2025-01-01" onChange={dateChanged}/>
             </div>
         </div>
         <div className="new-expense__actions">
