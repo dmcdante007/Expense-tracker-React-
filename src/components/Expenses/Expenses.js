2046d34d24c9aa49a89cad4822628ac92eee5354
dmcdante007 <meshramswaraj98>
2024-03-12 01:21:07 +0530

    "Added-expense-bar"

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
-
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear ] = useState("2024");
@@ -23,7 +23,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
       >
       </ExpensesFilter>
-
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList item={filteredExpenses}></ExpensesList>
 
       {/* <ExpenseItem
